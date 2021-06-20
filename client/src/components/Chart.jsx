2370de9140247e39a2be407d3a5ab3c00b164db3
Michael Rosen <mrosen86@gmail.com>
2021-06-20 16:24:36 -0700

    more animations

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
+import { motion } from 'framer-motion';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -9,25 +10,57 @@ const Metric = ({ metrics }) => {
   const style = {color, fontSize: '2rem'};
   return (
     <div className="metrics">
-      <div className="metric">
+      <motion.div
+        className="metric"
+        whileHover={{
+              rotate: [0, 360],
+              filter: [
+                'hue-rotate(0) contrast(100%)',
+                'hue-rotate(360deg) contrast(200%)',
+                'hue-rotate(45deg) contrast(300%)',
+                'hue-rotate(0) contrast(100%)',
+              ],
+              transition: {
+                duration: .8
+              }
+             }}
+      >
         <CircularProgressbarWithChildren value={metrics.valence * 100}>
           <img style={{ width: 40, marginTop: -5 }} src="/emotions.png" alt="emo" />
           <div style={{ fontSize: 12, marginTop: -5 }}>
             <strong>{metrics.valence * 100} % </strong> happy
           </div>
         </CircularProgressbarWithChildren>
-      </div>
-      <div className="metric">
+      </motion.div>
+      <motion.div
+        className="metric"
+        whileHover={{
+              rotate: [0, 360],
+              filter: [
+                'hue-rotate(0) contrast(100%)',
+                'hue-rotate(360deg) contrast(200%)',
+                'hue-rotate(45deg) contrast(300%)',
+                'hue-rotate(0) contrast(100%)',
+              ],
+              transition: {
+                duration: .8
+              }
+             }}
+      >
       <CircularProgressbarWithChildren value={metrics.energy * 100}>
         <img style={{ width: 40, marginTop: -5 }} src="/flash.png" alt="doge" />
         <div style={{ fontSize: 12, marginTop: -5 }}>
           <strong>{metrics.energy * 100} % </strong> energy
         </div>
       </CircularProgressbarWithChildren>
-    </div>
+    </motion.div>
     <span>
       <p>The dominant color is: </p>
-      <p style={style}><strong>{color}</strong></p>
+      <motion.p
+        style={style}
+      >
+        <strong>{color}</strong>
+      </motion.p>
     </span>
     </div>
   )
